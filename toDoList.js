d19482180caf87d827534db53de079362fb097ee
Kavishka Gamage <43032723+codekcg23@users.noreply.github.com>
2019-09-20 02:09:56 +0530

    Update toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -6,14 +6,14 @@ var myList =  document.getElementById('myList');
 
 textField.focus();
 
-btnCreate.addEventListener("click",(e)=>{   // when press 'create item'
+btnCreate.addEventListener("click",(e)=>{                                    // when press 'create item'
 
-    e.preventDefault();               //stop refreshing to prevent data lost
+    e.preventDefault();                                                     //stop refreshing to prevent data lost
     createItem(textField.value);
 
 })
 
-textField.addEventListener("keypress",function(keyPressed){                            //when press enter key
+textField.addEventListener("keypress",function(keyPressed){                 //when press enter key
 
     if(keyPressed.which == 13){
 
@@ -22,18 +22,18 @@ textField.addEventListener("keypress",function(keyPressed){                     
     }
 })  
 
-function createItem(x){                           // when press 'create item'
+function createItem(x){                                                      // when press 'create item'
     
     var myHTML =`<li>${x}<button onclick='deleteItem(this)'>Delete</button></li>`;
-    myList.insertAdjacentHTML("beforeend",myHTML);               //add another item after one
+    myList.insertAdjacentHTML("beforeend",myHTML);                           //add another item after one
     textField.value =" ";
-    textField.focus();              //focus cursor on text field
+    textField.focus();                                                       //focus cursor on text field
 
 }
 
 function deleteItem(item){
 
-    item.parentElement.remove();    //delete burron parent element is list
+    item.parentElement.remove();                                             //delete burron parent element is list
     
     
 } 
